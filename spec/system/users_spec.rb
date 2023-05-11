7363026f9e5a8401266279fa48273a485136c186
Your Name <you@example.com>
2023-05-11 13:16:16 +0900

    fix spec

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -7,12 +7,13 @@ RSpec.describe "Users", type: :system do
     # Arrange
     before do
       User.create(username: "test", email: "test@example.com", password: "password")
-      visit new_user_session_path
     end
 
     context 'ログインに成功した場合' do
       it 'ログインできたことを知らせる' do
         # Act
+        visit new_user_session_path
+
         fill_in 'Email', with: "test@example.com"
         fill_in "Password", with: "password"
         click_button 'Log in'
@@ -24,6 +25,8 @@ RSpec.describe "Users", type: :system do
     context 'ログインに失敗した場合' do
       it 'ログイン情報に誤りがあることを知らせる' do
         # Act
+        visit new_user_session_path
+
         fill_in 'Email', with: "test@example.com"
         fill_in 'Password', with: 'invalid_password'
         click_button 'Log in'
@@ -34,15 +37,12 @@ RSpec.describe "Users", type: :system do
   end
 
 
-  describe "新規登録処理" do
-    # Arrange
-    before do
-      visit new_user_registration_path
-    end
-    
+  describe "新規登録処理" do    
     context "新規登録に成功した場合" do
       it "新規登録成功後の遷移先が正しい" do
         # Act
+        visit new_user_registration_path
+
         fill_in "Username", with: "test"
         fill_in "Email", with: "test@example.com"
         fill_in "Password", with: "password", match: :first
@@ -56,6 +56,8 @@ RSpec.describe "Users", type: :system do
     context "新規登録に失敗した場合" do      
       it "新規登録失敗後の遷移先が正しい" do
         # Act
+        visit new_user_registration_path
+
         fill_in "Username", with: "test"
         fill_in "Email", with: "test@example.com"
         fill_in "Password", with: "password", match: :first
