e98d3cd003c61c92d6a946b9e8c280caa3f96dda
Your Name <you@example.com>
2023-05-11 11:53:39 +0900

    fix spec

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,65 +3,68 @@ require 'rails_helper'
 RSpec.describe "Users", type: :system do
   
 
-  describe 'ログイン' do
+  describe 'ログイン処理' do
+    # Arrange
     before do
       User.create(username: "test", email: "test@example.com", password: "password")
       visit new_user_session_path
     end
 
-    context 'ログイン情報を入力するとき' do
-      it 'ログインできること' do
+    context 'ログインに成功した場合' do
+      it 'ログインできたことを知らせる' do
+        # Act
         fill_in 'Email', with: "test@example.com"
         fill_in "Password", with: "password"
         click_button 'Log in'
+        # Assert
         expect(page).to have_content 'Signed in successfully.'
       end
+    end
 
-      it '不正なログイン情報を入力するとき' do
+    context 'ログインに失敗した場合' do
+      it 'ログイン情報に誤りがあることを知らせる' do
+        # Act
         fill_in 'Email', with: "test@example.com"
         fill_in 'Password', with: 'invalid_password'
         click_button 'Log in'
+        # Assert
         expect(page).to have_content 'Invalid Email or password.'
       end
     end
   end
 
 
-  describe "新規登録" do
+  describe "新規登録処理" do
+    # Arrange
     before do
       visit new_user_registration_path
     end
     
     context "新規登録に成功した場合" do
-      before do
+      it "新規登録成功後の遷移先が正しい" do
+        # Act
         fill_in "Username", with: "test"
         fill_in "Email", with: "test@example.com"
         fill_in "Password", with: "password", match: :first
         fill_in "Password confirmation", with: "password", match: :first
         click_button "Sign up"
-      end
-
-      it "新規登録成功後の遷移先が正しいこと" do
+        # Assert
         expect(page).to have_current_path root_path
       end
     end
 
-    context "新規登録に失敗した場合" do
-      before do
+    context "新規登録に失敗した場合" do      
+      it "新規登録失敗後の遷移先が正しい" do
+        # Act
         fill_in "Username", with: "test"
         fill_in "Email", with: "test@example.com"
         fill_in "Password", with: "password", match: :first
         fill_in "Password confirmation", with: "password2", match: :first
         click_button "Sign up"
-      end
-            
-      it "新規登録失敗後の遷移先が正しいこと" do
+        # Assert
         expect(page).to have_current_path "/users"
       end
-
-      
     end
-    
   end
-
+  
 end
