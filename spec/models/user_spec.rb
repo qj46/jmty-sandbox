a7071fa6b9981f391832054130a5c1ad84b80bfc
Your Name <you@example.com>
2023-05-11 15:40:17 +0900

    fix model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,53 +3,59 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'validations' do
-    context 'when full_name exceeds maximum length' do
-      let(:user) { build(:user, full_name: Faker::Lorem.characters(number: 51)) }
-
-      it 'is invalid' do
+  
+  describe 'validation' do
+    context '登録出来ない場合' do
+      it '使えないアカウント名は登録出来ない' do
+        # Act, Assert
+        
+        # admin
+        user = build(:user, username: "admin")
         expect(user).not_to be_valid
-        expect(user.errors[:full_name]).to include('is too long (maximum is 50 characters)')
-      end
-    end
 
-    context 'when body exceeds maximum length' do
-      let(:user) { build(:user, body: Faker::Lorem.characters(number: 81)) }
-
-      it 'is invalid' do
+        # root
+        user = build(:user, username: "root")
         expect(user).not_to be_valid
-        expect(user.errors[:body]).to include('is too long (maximum is 80 characters)')
-      end
-    end
 
-    context 'when username is invalid' do
-      before { create(:user, username: 'taken_username') }
+        # dashboard
+        user = build(:user, username: "dashboard")
+        expect(user).not_to be_valid
 
-      # it 'should be invalid if username is already taken' do
-      #   user = build(:user, username: 'taken_username')
-      #   expect(user).not_to be_valid
-      #   expect(user.errors[:username]).to include('is already taken')
-      # end
+        # analytics
+        user = build(:user, username: "analytics")
+        expect(user).not_to be_valid
 
-      it 'should be invalid if username is restricted' do
-        restricted_usernames = %w[admin root dashboard analytics appearance settings preferences calendar]
+        # appearance
+        user = build(:user, username: "appearance")
+        expect(user).not_to be_valid
 
-        restricted_usernames.each do |username|
-          user = build(:user, username:)
-          expect(user).not_to be_valid
-          expect(user.errors[:username]).to include('is restricted')
-        end
+        # settings
+        user = build(:user, username: "settings")
+        expect(user).not_to be_valid
+
+        # preferences
+        user = build(:user, username: "preferences")
+        expect(user).not_to be_valid
+
+        # calendar
+        user = build(:user, username: "calendar")
+        expect(user).not_to be_valid
       end
     end
   end
 
   describe 'friendly_id' do
-    let(:user) { create(:user, username: 'test-username') }
+    # Arrange
+    let(:user) { create(:user, username: 'test') }
 
-    it 'should generate slug when username changes' do
-      expect(user.slug).to eq('test-username')
-      user.update(username: 'new-test-username')
-      expect(user.slug).to eq('new-test-username')
+    context 'ユーザー名が変わる場合' do
+      it 'ユーザー名とスラッグが変わる' do
+        # Act        
+        user.update(username: 'new-test')
+
+        # Assert
+        expect(user.slug).to eq('new-test')
+      end
     end
   end
 end
