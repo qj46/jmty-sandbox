926301124fa8509bd65e0669f2a513c2619d8ca4
Your Name <you@example.com>
2023-05-12 09:30:55 +0900

    add helpers

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module AdminHelper
+
+  def local_user_name
+    User.model_name.human
+  end
+
+  def local_user_column_name(column)
+    User.human_attribute_name(column)
+  end
+
 end
