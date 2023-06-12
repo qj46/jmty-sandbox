bd1fab8383df3be50b137bab015b92f8beb43aae
Your Name <you@example.com>
2023-06-13 06:03:12 +0900

    DM機能

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -16,29 +16,29 @@ class DashboardController < ApplicationController
 
   def show
     @should_render_navbar = true
-    redirect_to dashboard_path if @user.nil?
+    redirect_to dashboard_path if @user.nil? # MEMO @userが存在しない場合、リダイレクト
     # @links = @user.links.where.not(url: '', title: '')
 
+    # MEMO 以下全てDM機能
+    return unless current_user # MEMO ログインしていない場合、処理を終了
 
-    return unless current_user
-
-    @current_entry = current_user.entries
-    @another_entry = @user.entries
+    current_entry = current_user.entries
+    another_entry = @user.entries
     return if @user.id == current_user.id
 
-    @current_entry.each do |current|
-      @another_entry.each do |another|
+    current_entry.each do |current|
+      another_entry.each do |another|
         if current.room_id == another.room_id
-          @is_room = true
-          @room_id = current.room_id
+          @is_room = true # MEMO dashboard/show.html.erb の <% if @is_room %>でインスタンス変数使用
+          @room_id = current.room_id # MEMO dashboard/show.html.erb の <%= link_to 'DM', room_path(@room_id) %>でインスタンス変数使用
         end
       end
     end
 
-    return if @is_room
-
-    @room = Room.new
-    @entry = Entry.new
+    return if @is_room # MEMO 既にroomがある場合、処理を終了
+    @room = Room.new # MEMO dashboard/show.html.erb の <%= form_for @room do |f| %>でインスタンス変数使用
+    @entry = Entry.new # MEMO dashboard/show.html.erb の <%= fields_for @entry do |e| %>でインスタンス変数使用
+                       # MEMO RoomsController の another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: room.id)に渡す
   end
 
   private
